03c492a8b38e81cc94e1740051666357dde66618
Antonio Barreiro <antonio.barreiro.dw@immune.institute>
2024-11-12 20:36:42 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
-
 const logger = require('morgan');
-
 const cookieParser = require('cookie-parser');
-
 const cors = require('cors');
 
 const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:3000';
@@ -11,19 +8,19 @@ const FRONTEND_URL = process.env.ORIGIN || 'http://localhost:3000';
 module.exports = (app) => {
   app.set('trust proxy', 1);
 
+ 
   app.use(
     cors({
-      origin: [FRONTEND_URL],
+      origin: FRONTEND_URL,
       credentials: true,
     })
   );
 
   app.use(logger('dev'));
-
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cookieParser());
 
+ 
   require('../passport')(app);
-
 };
